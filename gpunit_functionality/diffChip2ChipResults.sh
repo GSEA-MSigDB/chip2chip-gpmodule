07755e795588e1dae5d3ad1876adf8e4e350b3d7
eby <eby@broadinstitute.org>
2018-08-08 12:54:15 +0900

    Added GpUnit test suite

---
+++ gpunit_functionality/diffChip2ChipResults.sh
@@ -0,0 +1,19 @@
+# Copyright (c) 2003-2017 Broad Institute, Inc., Massachusetts Institute of Technology, and Regents of the University of California.  All rights reserved.
+#!/bin/sh
+execDir=`dirname $0`
+
+base1=`basename $1`
+base2=`basename $2`
+diffDir1=`mktemp -d $base1.XXXXXX`
+diffDir2=`mktemp -d $base2.XXXXXX`
+
+# The generated GMT/GMX files sometimes have a different order when there are multiple GMT inputs.
+# This should probably be changed in the long run, but for now we just compare the sorted outputs
+sort $1 > $diffDir1/sorted.matrix
+sort $2 > $diffDir2/sorted.matrix
+
+diff --strip-trailing-cr -q $diffDir1/sorted.matrix $diffDir2/sorted.matrix
+status=$?
+
+rm -rf $diffDir1 $diffDir2
+exit $status
---
+++ gpunit_functionality/diffChip2ChipResultsAndReports.sh
@@ -0,0 +1,46 @@
+# Copyright (c) 2003-2017 Broad Institute, Inc., Massachusetts Institute of Technology, and Regents of the University of California.  All rights reserved.
+#!/bin/sh
+execDir=`dirname $0`
+
+zip1=$1
+zip2=$2
+
+base1=`basename $1`
+base2=`basename $2`
+bare1=${base1%%.zip}
+bare2=${base2%%.zip}
+diffDir1=`mktemp -d $bare1.XXXXXX`
+diffDir2=`mktemp -d $bare2.XXXXXX`
+
+unzip -q $zip1 -d $diffDir1
+unzip -q $zip2 -d $diffDir2
+
+# The generated GMT/GMX files sometimes have a different order when there are multiple GMT inputs.
+# This should probably be changed in the long run, but for now we just compare the sorted outputs
+sort $diffDir1/*.gm? > $diffDir1/sorted.matrix
+sort $diffDir2/*.gm? > $diffDir2/sorted.matrix
+
+diff --strip-trailing-cr -q $diffDir1/sorted.matrix $diffDir2/sorted.matrix
+status=$?
+
+# Likewise for the XLS file
+sort $diffDir1/*.xls > $diffDir1/sorted.xls
+sort $diffDir2/*.xls > $diffDir2/sorted.xls
+
+diff --strip-trailing-cr -q $diffDir1/sorted.xls $diffDir2/sorted.xls
+status=$(( $? + status ))
+
+# Checking etiology TXT report files.  We're working out of diffDir1 as that is from the 'expected' result.
+txtFileList=$diffDir1/*.txt
+for txtFile in $txtFileList; do
+   baseTxtFile=`basename $txtFile`
+   if [ -s $diffDir2/$baseTxtFile ]; then
+      diff --strip-trailing-cr -q $txtFile $diffDir2/$baseTxtFile
+      status=$(( $? + status ))
+   else
+      status=$(( 1 + status ))
+   fi
+done
+
+rm -rf $diffDir1 $diffDir2
+exit $status
